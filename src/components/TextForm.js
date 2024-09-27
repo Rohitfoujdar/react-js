23ad20c456576db9592d951cf48b360697f95962
Rohitfoujdar <rohitfoujdar8696@gmail.com>
2024-09-27 21:20:36 +0530

    ftch

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -16,7 +16,7 @@ export default function TextForm(props) {
   return (
     <div>
         <h1>{props.heading}</h1>
-      <div className="mb-3">
+      <div className="my-3">
   <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="3"></textarea>
        </div>
        <button className='btn.btn-primary' onClick={handleUpClick}>Convert to upperCase</button>
